818a9a0ce108ccc4a63b8732d2cd4a116c10ab0e
lukabekavac <luka.bekavac@student.unisg.ch>
2023-05-10 11:01:38 +0200

    Ready to play

--- was-tournament/was_tournament.py
+++ was-tournament/was_tournament.py
@@ -1,10 +1,10 @@
 import axelrod as axl
-from wasstrategies import danai_strategy as pl0
+from wasstrategies import luka_strategy as pl0
 
 """Run an Axelrod Tournament (https://axelrod.readthedocs.io/en/stable/tutorials/new_to_game_theory_and_or_python/tournament.html)"""
 # Create the strategy players
 strategies = [axl.Cooperator(), axl.Defector(),
-axl.TitForTat(), axl.Grudger(), pl0.Danai()]
+axl.TitForTat(), axl.Grudger(), pl0.Luka()]
 
 # Print the startegy players
 print(f"Available strategies: {strategies}")
@@ -16,7 +16,7 @@ prisoners_dilemma = axl.game.Game(r=-1, s=-5, t=0, p=-3)
 tournament = axl.Tournament(strategies, game = prisoners_dilemma, turns=10, repetitions=1)
 
 # Play the tournament
-results = tournament.play(filename="was-tournament/was_tournament_analysis.csv")
+results = tournament.play(filename="/Users/lukabekavac/PycharmProjects/axelrod-tournament-web-based/was-tournament/was_tournament_analysis.csv")
 
 # Print the players from the best to the worst
 print(f"\nRanked results: {results.ranked_names}\n")
@@ -28,17 +28,17 @@ plot = axl.Plot(results)
 # View the scores averaged per opponent and turns
 p1 = plot.boxplot()
 p1.show()
-p1.savefig('was-tournament/was_results.png')
+p1.savefig('/Users/lukabekavac/PycharmProjects/axelrod-tournament-web-based/was-tournament/was_results.png')
 
 # View the distributions of wins for each strategy
 p2 = plot.winplot()
 p2.show()
-p2.savefig('was-tournament/was_win_distributions.png')
+p2.savefig('/Users/lukabekavac/PycharmProjects/axelrod-tournament-web-based/was-tournament/was_win_distributions.png')
 
 # View the payoff matrix
 p2 = plot.payoff()
 p2.show()
-p2.savefig('was-tournament/was_payoff_matrix.png')
+p2.savefig('/Users/lukabekavac/PycharmProjects/axelrod-tournament-web-based/was-tournament/was_payoff_matrix.png')
 
 
 """Morality metrics (https://axelrod.readthedocs.io/en/latest/how-to/calculate_morality_metrics.html#morality-metrics)"""
